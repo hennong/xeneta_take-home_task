d3ff1e949315e241146dbeb61d86260eb9d5901d
Henning <henning.koenigs@gmx.de>
2023-01-29 20:54:15 +0100

    provide Store to project

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,10 +1,16 @@
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { App } from './App'
+import { store } from './Store/Store'
 
 const rootElement = document.getElementById('root')
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement)
 
-  root.render(<App />)
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
 }
