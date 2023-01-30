3993ee09c48d171b4c63eae868b89067ada515b6
Henning <henning.koenigs@gmx.de>
2023-01-30 03:03:42 +0100

    remove TODO comment

--- src/Components/SearchBar/SearchBar.styles.ts
+++ src/Components/SearchBar/SearchBar.styles.ts
@@ -1,6 +1,5 @@
 export const searchBarStyles: React.CSSProperties = {
   height: '7vh',
-  //TODO: actually use Theme here
   backgroundColor: '#00a5cf',
   display: 'flex',
   justifyContent: 'center',
